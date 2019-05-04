889f947c76f005a94f85ec3e707fbb4e57cf5ce2
HuyVo1428 <40543421+HuyVo1428@users.noreply.github.com>
2019-05-04 16:12:18 +0700

    Update 190504
    Them noi dung cau tra loi

--- Task3.2/js/action.js
+++ Task3.2/js/action.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
         $(this).tab('show');
     });
 });
+
+function startTime(){
+    var asiaTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Ho_Chi_Minh"});
+    asiaTime = new Date(asiaTime);
+
+    var year=asiaTime.getFullYear();
+    var month=asiaTime.getMonth();
+    var day=asiaTime.getDate();
+    var hours=asiaTime.getHours();
+    var minutes=asiaTime.getMinutes();
+    var seconds=asiaTime.getSeconds();
+
+
+    var result=year+" - " + month + " - " + day + "   " + hours + ":" + minutes + ":" + seconds + "   GMT+7";
+
+    document.getElementById("question4_2").innerHTML = result;
+    var t = setTimeout(startTime, 500);
+}
