a36f5ff8f967cc876b88bbab25c6534a66259644
HuyVo1428 <40543421+HuyVo1428@users.noreply.github.com>
2019-05-04 22:52:15 +0700

    Update 190504
    Hoan thanh cau 4

--- Task3.2/js/action.js
+++ Task3.2/js/action.js
@@ -4,20 +4,57 @@ $(document).ready(function() {
     });
 });
 
-function startTime(){
-    var asiaTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Ho_Chi_Minh"});
+function startTime() {
+    var asiaTime = new Date().toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
     asiaTime = new Date(asiaTime);
 
-    var year=asiaTime.getFullYear();
-    var month=asiaTime.getMonth();
-    var day=asiaTime.getDate();
-    var hours=asiaTime.getHours();
-    var minutes=asiaTime.getMinutes();
-    var seconds=asiaTime.getSeconds();
+    var year = asiaTime.getFullYear();
+    var month = asiaTime.getMonth();
+    var day = asiaTime.getDate();
+    var hours = asiaTime.getHours();
+    var minutes = asiaTime.getMinutes();
+    var seconds = asiaTime.getSeconds();
 
 
-    var result=year+" - " + month + " - " + day + "   " + hours + ":" + minutes + ":" + seconds + "   GMT+7";
+    var result = year + " - " + month + " - " + day + "   " + hours + ":" + minutes + ":" + seconds + "   GMT+7";
 
     document.getElementById("question4_2").innerHTML = result;
     var t = setTimeout(startTime, 500);
+}
+
+function addRanNum() {
+    var num1 = Math.floor(Math.random() * 100) + 1;
+    var num2 = Math.floor(Math.random() * 100) + 1;
+
+    var result = num1 + num2;
+
+    document.getElementById("question4_3").innerHTML = num1 + " " + "+" + " " + num2 + " " + "=" + " " + result;
+
+    var t = setTimeout(addRanNum, 5000);
+}
+
+var flagChangeBackgroundImg = 0;
+
+function changeBackgroundImg() {
+    document.body.style.backgroundColor = null;
+    if (flagChangeBackgroundImg == 0) {
+        document.body.style.backgroundImage = "url('img/img1.jpg')";
+        flagChangeBackgroundImg = 1;
+    } else {
+        document.body.style.backgroundImage = "url('img/img2.jpg')";
+        flagChangeBackgroundImg = 0;
+    }
+}
+
+var flagChangeBackgroundColor = 0;
+
+function changeColorBackground() {
+    document.body.style.backgroundImage = null;
+    if (flagChangeBackgroundColor == 0) {
+        document.body.style.backgroundColor = "LightGray";
+        flagChangeBackgroundColor = 1;
+    } else {
+        document.body.style.backgroundColor = "LightSteelBlue";
+        flagChangeBackgroundColor = 0;
+    }
 }
